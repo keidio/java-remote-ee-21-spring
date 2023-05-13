98931fa4dd77f63bd934295049f3b7332e14e6bb
Keidi Oras <keidioras@gmail.com>
2023-05-13 13:12:33 +0300

    try-login endpoint with GET http method

--- src/main/java/com/sda/javaremoteee21spring/rest/MyFirstController.java
+++ src/main/java/com/sda/javaremoteee21spring/rest/MyFirstController.java
@@ -1,16 +1,22 @@
 package com.sda.javaremoteee21spring.rest;
 
 import com.sda.javaremoteee21spring.dto.Person;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class MyFirstController {
+
+
+    //SLf4j, same as:
+    //private static final Logger log = LoggerFactory.getLogger(MyFirstController.class);
 
     @GetMapping("/name")
     public String showMeMyName(){
@@ -23,9 +29,10 @@ public class MyFirstController {
     }
 
     @GetMapping("me-and-friend")
+    //@GetMapping(value="me-and-friend")
     public List<Person> meAndFriend(){
 
-        List<Person> persons = new ArrayList<>();
+        //List<Person> persons = new ArrayList<>();
         //persons.add(...);
         //persons.add(...);
         return List.of(
@@ -33,4 +40,26 @@ public class MyFirstController {
                 new Person("John", "D.", 30)
         );
     }
+
+    //?param1=value1&param2=value2
+    // /api/try-login?login=keidi&password=12345
+
+    //@RequestParam(required = false) String password or with/without defaultvalue
+    @GetMapping("/try-login")
+    public String logIn(@RequestParam(defaultValue = "John Doe", value= "login") String userLogin,
+                        @RequestParam(defaultValue = "pass", value = "password") String userPassword) {
+
+        //string interpolation
+        log.info("received login: [{}] and password [{}]", userLogin, userPassword);
+        return "OK";
+    }
 }
+
+    /*@GetMapping("/try-login")
+    public String logIn(@RequestParam(defaultValue = "John Doe") String login, @RequestParam(defaultValue = "pass") String password) {
+
+        //string interpolation
+        log.info("received login: [{}] and password [{}]", userLogin, userPassword);
+        return "OK";
+    }
+}*/
