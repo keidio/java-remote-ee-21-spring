94046bc7c80681b86a232a1e0f9ccbc62b8d08e7
Keidi Oras <keidioras@gmail.com>
2023-05-20 10:25:25 +0300

    two endpoints for car implemented - get all cars and get one car

--- src/main/java/com/sda/javaremoteee21spring/controller/rest/CarController.java
+++ src/main/java/com/sda/javaremoteee21spring/controller/rest/CarController.java
@@ -4,10 +4,13 @@ import com.sda.javaremoteee21spring.entity.Car;
 import com.sda.javaremoteee21spring.service.CarService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RequestMapping("/api")
 @RestController
 @Slf4j
 public class CarController {
@@ -18,9 +21,16 @@ public class CarController {
         this.carService = carService;
     }
 
-    @GetMapping("/all-cars")
+    @GetMapping("/cars")
     public List<Car> getAllCars(){
         log.info("getting all cars");
         return carService.findAllCars();
     }
+
+    // dynamic id
+    @GetMapping("/cars/{id}")
+  public Car getCarById(@PathVariable Long id){
+        log.info("getting car by id: [{}]", id);
+        return carService.findCarById(id);
+  }
 }
