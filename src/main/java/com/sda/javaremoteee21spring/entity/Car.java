9e71a6ef35ecc7290a947d0c44ec672617e8e58a
Keidi Oras <keidioras@gmail.com>
2023-06-03 12:15:42 +0300

    Improving entity mapping of Car

--- src/main/java/com/sda/javaremoteee21spring/entity/Car.java
+++ src/main/java/com/sda/javaremoteee21spring/entity/Car.java
@@ -1,25 +1,34 @@
 package com.sda.javaremoteee21spring.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.YearMonth;
 
+@Entity
+@Table(name = "CARS")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder //
+@Builder //builder don't need no-args constructor
 public class Car {
+    @Id
+            @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
     String brand;
     String model;
     String vin;
+
+    @Enumerated(EnumType.STRING)
     Colour colour;
+
     YearMonth productionYearMonth;
     int mileage;
+
+    @Enumerated(EnumType.STRING)
     Fuel fuelType;
+
     boolean insurance;
 
 
--- src/main/java/com/sda/javaremoteee21spring/entity/Colour.java
+++ src/main/java/com/sda/javaremoteee21spring/entity/Colour.java
@@ -1,9 +1,26 @@
 package com.sda.javaremoteee21spring.entity;
 
 public enum Colour {
-    SOUL_RED,
-    COSMIC_WHITE,
-    ELECTRIC_BLUE,
-    MIDNIGHT_BLACK,
-    SPACE_GRAY
+    SOUL_RED("Nice red"),
+    COSMIC_WHITE("Metallic White"),
+    ELECTRIC_BLUE("Blue"),
+    MIDNIGHT_BLACK("Matte black"),
+    SPACE_GRAY("Metallic gray");
+
+
+    private String description;
+
+    public String getDescription(){
+        return description;
+    }
+    Colour(String description) {
+        this.description= description;
+    }
+
+    public static void main(String[] args) {
+        Colour soulRed = SOUL_RED;
+        System.out.println(soulRed.getDescription());
+        System.out.println("name is: " + soulRed.name());
+        System.out.println("Ordinal is: " + soulRed.ordinal());
+    }
 }
