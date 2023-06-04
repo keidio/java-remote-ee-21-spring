99ac5da0aac3a0e4b8c5f35118c10f60eafd3b6e
Keidi Oras <keidioras@gmail.com>
2023-06-04 13:08:56 +0300

    Second endpoint added to CarWebController

--- src/main/java/com/sda/javaremoteee21spring/controller/web/CarWebController.java
+++ src/main/java/com/sda/javaremoteee21spring/controller/web/CarWebController.java
@@ -1,18 +1,41 @@
 package com.sda.javaremoteee21spring.controller.web;
 
+import com.sda.javaremoteee21spring.service.CarService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @Slf4j
 @RequestMapping("/web")
 public class CarWebController {
-    @GetMapping({"/home-page", "/"})
-    public String homePage(){
+
+    private final CarService carService;
+
+    public CarWebController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping({"/home-page", ""})
+    public String homePage(Model data,
+                           @RequestParam(value = "name", defaultValue = "Keidi") String name,
+                           @RequestParam(value = "surname", defaultValue = "O.") String surname){
         log.info("Home page");
+
+        data.addAttribute("name", name);
+        data.addAttribute("surname", surname);
         return "home-page";
     }
 
+    @GetMapping("/cars")
+    public String allCars(Model data){
+        log.info("All cars at html page");
+
+        var allCars= carService.findAllCars();
+        data.addAttribute("cars", allCars);
+        return "all-cars";
+    }
 }
